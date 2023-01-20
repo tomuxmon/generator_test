e0960917b2bab5220ee999a2b295f45b88ce6122
Tomas Dambrauskas <tomas.dambrauskas@nordsec.com>
2023-01-20 07:59:13 +0200

    first commit

---
+++ receptor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("before");
+        HelloFrom("Generated Code");
+        Console.WriteLine("after");
+    }
+
+    static partial void HelloFrom(string name);
+}
